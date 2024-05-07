f30647c3dd9674e980bc5639c53364bd31d5d7fc
Dogan Ulus <dogan.ulus@bogazici.edu.tr>
2024-05-07 21:25:07 +0300

    Add random obstacles

--- python/project.py
+++ python/project.py
@@ -1,5 +1,7 @@
 import time
 import random
+import numpy as np
+import scipy as sp
 
 import mujoco
 import mujoco.viewer
@@ -42,10 +44,15 @@ def create_scenario():
     # Add the robot to the scene.
     robot, robot_assets = cmpe434_utils.get_model('models/mushr_car/model.xml')
     start_pos = random.choice([key for key in tiles.keys() if tiles[key] == "."])
-    final_pos = random.choice([key for key in tiles.keys() if tiles[key] == "."])
+    final_pos = random.choice([key for key in tiles.keys() if tiles[key] == "." and key != start_pos])
 
     scene.worldbody.add('site', name='start', type='box', size=[0.5, 0.5, 0.01], rgba=[0, 0, 1, 1],  pos=[start_pos[0]*2, start_pos[1]*2, 0])
     scene.worldbody.add('site', name='finish', type='box', size=[0.5, 0.5, 0.01], rgba=[1, 0, 0, 1],  pos=[final_pos[0]*2, final_pos[1]*2, 0])
+
+    # create obstables for each room
+    for i, room in enumerate(rooms):
+        obs_pos = random.choice([tile for tile in room if tile != start_pos and tile != final_pos])
+        scene.worldbody.add('geom', name='Z{}'.format(i), type='cylinder', size=[0.2, 0.05], rgba=[0.8, 0.0, 0.1, 1],  pos=[obs_pos[0]*2, obs_pos[1]*2, 0.08])
 
     start_yaw = random.randint(0, 359)
     robot.find("body", "buddy").set_attributes(pos=[start_pos[0]*2, start_pos[1]*2, 0.1], euler=[0, 0, start_yaw])
@@ -61,6 +68,14 @@ def execute_scenario(scene, ASSETS=dict()):
 
     m = mujoco.MjModel.from_xml_string(scene.to_xml_string(), assets=all_assets)
     d = mujoco.MjData(m)
+
+    rooms = [m.geom(i).id for i in range(m.ngeom) if m.geom(i).name.startswith("R")]
+    obstacles = [m.geom(i).id for i in range(m.ngeom) if m.geom(i).name.startswith("Z")]
+
+    uniform_direction_dist = sp.stats.uniform_direction(2)
+    obstacle_direction = [[x, y, 0] for x,y in uniform_direction_dist.rvs(len(obstacles))]
+
+    unused = np.zeros(1, dtype=np.int32)
 
     with mujoco.viewer.launch_passive(m, d, key_callback=key_callback) as viewer:
 
@@ -78,6 +93,24 @@ def execute_scenario(scene, ASSETS=dict()):
             if not paused:
                 # velocity.ctrl = 1.0 # update velocity control value
                 # steering.ctrl = 4.0 # update steering control value
+
+                # obstable update
+                for i, x in enumerate(obstacles):
+                    dx = obstacle_direction[i][0]
+                    dy = obstacle_direction[i][1]
+
+                    px = m.geom_pos[x][0]
+                    py = m.geom_pos[x][1]
+                    pz = 0.02
+
+                    nearest_dist = mujoco.mj_ray(m, d, [px, py, pz], obstacle_direction[i], None, 1, -1, unused)
+
+                    if nearest_dist >= 0 and nearest_dist < 0.4:
+                        obstacle_direction[i][0] = -dy
+                        obstacle_direction[i][1] = dx
+
+                    m.geom_pos[x][0] = m.geom_pos[x][0]+dx*0.001
+                    m.geom_pos[x][1] = m.geom_pos[x][1]+dy*0.001
 
                 # mj_step can be replaced with code that also evaluates
                 # a policy and applies a control signal before stepping the physics.
